670e049d9c8bad8ffcba3cc0903eed670c735377
megz15 <56220472+megz15@users.noreply.github.com>
2020-08-14 23:46:29 +0530

    GUI!!
    Direly needs cosmetic improvements

--- WhatStats.py
+++ WhatStats.py
@@ -1,102 +1,120 @@
-#V_1.0 by Meghraj Goswami
-#***************************************************
+#---Import Libraries Create TK window & Declare Variables---#
+from tkinter import * 
+from tkinter.ttk import *
+from tkinter.filedialog import askopenfile
 import matplotlib.pyplot as plt
+import matplotlib.ticker as tick
 import datetime
-try:
-    fname = input("Enter full path to txt file: ")
-    f = open(fname,encoding="utf-8")
-    g = open(fname,encoding="utf-8")
-    h = open(fname,encoding="utf-8")
-except FileNotFoundError:
-    print("File not found, try again")
-    exit()
-dates_list, date_names, date_types , user_dict, tot_dates, test_lst = [], [], [], {}, {}, []
-#***************************************************
-days_not_months=[13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-for l in h:
-    if l.find(':',17)==-1:
-        continue
-    try:
-        test_lst.append(int(l[0:l.find('/')]))
-    except ValueError:
-        continue
 
-for i in days_not_months:
-    if i in test_lst:
-        date_types=['%d/%m/%y','%d/%m/%Y']
+root = Tk() 
+root.geometry('200x100')
+
+days_not_months = [13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
+dates_list, date_names, date_types, d_or_m = [],[],[],[]
+tot_ints, user_dict = {},{}
+
+#---Open Text File---#
+def open_file():
+    global f
+    file = askopenfile(mode='r',filetypes=[('WA Chat Export Text Files','*.txt')],title='Open WhatsApp Chat Export txt file')
+    if file is not None:
+        f=open(file.name,encoding='utf-8')
+        g=open(file.name,encoding='utf-8')
+    for l in f:
+        if l.find(': ',l.find(':')+1)==-1:
+            continue
+        else:
+            try:
+                d_or_m.append(int(l[:2:].replace('-','').replace('/','').replace('.',''))) #Get first two digits to check day or month (>12, not month)
+            except ValueError:
+                continue
+    for k in g:
+        if k.find(': ',k.find(':')+1)==-1:
+            continue
+        else:
+            try:
+                abc=k[k.find('- ')+2:k.find(': '):]
+                q=k[:k.find(', ')]
+                if calc_date(q)!=None:
+                    dates_list.append(calc_date(q)) #Get time
+                    date_names.append(calc_date(q)+abc)
+                else:
+                    continue
+                if abc in list(user_dict.keys()):
+                    pass
+                else:
+                    user_dict[abc]={}
+            except ValueError:
+                continue
+    for i in list(dict.fromkeys(date_names)):
+        for j in list(user_dict.keys()):
+            res = [x.isdigit() for x in i[::-1]].index(True)
+            if j==i[len(i)-res::]:
+                for za in list(calc_interval(dates_list).keys()):
+                    if i[:len(i)-res:]==za:
+                        user_dict[j].update({za:date_names.count(i)})
+            
+
+#---""---#
+def plot_graph():
+    y=[]
+    for i in tot_ints:
+        y.append(dates_list.count(i))
+    if (f.name).find("WhatsApp Chat with ")!=-1:
+        t0 = (f.name)[(f.name).find("WhatsApp Chat with ")::]
+        t0 = t0.replace('Chat with','ChatStats for').rstrip('.txt')
+        t0+='\n'
     else:
-        date_types=['%m/%d/%y','%m/%d/%Y']
+        t0 = 'WhatStats\n'
 
-def find_date(o_date):
+    plt.figure('WhatStats')
+
+    plt.axes().xaxis.set_major_locator(tick.MultipleLocator(int(txt_divs.get())))
+    plt.axes().xaxis.set_minor_locator(tick.MultipleLocator(1))
+
+    plt.plot(list(tot_ints.keys()),y,label="Total",alpha=0.2)
+
+    for i in user_dict:
+        tot_ints_copy=tot_ints.copy()
+        tot_ints_copy.update(user_dict[i])
+        plt.plot(list(tot_ints_copy.keys()),list(tot_ints_copy.values()),label=i)
+    plt.xticks(rotation=45)
+    plt.ylabel('Frequency of messages')
+    if len(user_dict)<=10:
+        t0+="Members: "+str(list(user_dict.keys())).lstrip('[').rstrip(']').replace("'",'')
+        plt.legend()
+    else:
+        t0+=str(len(user_dict))+' Members in Group'
+        plt.legend(ncol=round(len(user_dict)/20),fontsize=8)
+    plt.title(t0)
+    plt.show()
+
+#---Calculate Date Format Automatically---#
+def calc_date(o_date):
+    if any(x in days_not_months for x in d_or_m):
+        date_types=['%d/%m/%y','%d/%m/%Y','%d-%m-%y','%d-%m-%Y','%d.%m.%y','%d.%m.%Y','%d/%m/%y','%d/%m/%Y','%d-%m-%y','%d-%m-%Y','%d.%m.%y','%d.%m.%Y']
+    else:
+        date_types=['%m/%d/%y','%m/%d/%Y','%m-%d-%y','%m-%d-%Y','%m.%d.%y','%m.%d.%Y','%m/%d/%y','%m/%d/%Y','%m-%d-%y','%m-%d-%Y','%m.%d.%y','%m.%d.%Y']
     for typ in date_types:
         try:
-            a=datetime.datetime.strptime(o_date,typ).date()
-            return a.strftime('%b%d,%y')
+            a=datetime.datetime.strptime(o_date,typ)
+            return(a.strftime('%b%d,%y'))
         except ValueError:
-            pass
-
-for j in f:
-    if j.find(':',17)==-1:
-        continue
-    try:
-        abc=j[j.find('- ')+2:j.find(': '):]
-        a=j[0:j.find(',')]
-        if find_date(a)!=None:
-            dates_list.append(find_date(a))
-            date_names.append(find_date(a)+abc)
-        else:
             continue
-        if abc in list(user_dict.keys()):
-            pass
-        else:
-            user_dict[abc]={}
-    except ValueError:
-        continue
-#***************************************************
-fdate = datetime.datetime.strptime(dates_list[0],'%b%d,%y')
-ldate = datetime.datetime.strptime(dates_list[-1],'%b%d,%y')
-diff = ldate - fdate
-for i in range(diff.days + 1):
-    day=(fdate + datetime.timedelta(days=i))
-    tot_dates[day.strftime('%b%d,%y')]=0
-#***************************************************
-z=[]
-for i in g:
-    if i.find(':',17)==-1:
-        continue
-    try:
-        for j in list(user_dict.keys()):
-            temp_i=i[i.find('- ')+2:i.find(': '):]
-            date_i=i[0:i.find(',')]
-            if temp_i==j:
-                z.append(find_date(date_i)+j)
-    except ValueError:
-        continue
-for i in list(dict.fromkeys(z)):
-    for j in list(user_dict.keys()):
-        res = [x.isdigit() for x in i[::-1]].index(True)
-        if j==i[len(i)-res::]:
-            for za in list(tot_dates.keys()):
-                if i[:len(i)-res:]==za:
-                    user_dict[j].update({za:date_names.count(i)})
-#***************************************************
-y=[]
-for i in tot_dates:
-    y.append(dates_list.count(i))
-if (f.name).find("WhatsApp Chat with ")!=-1:
-    t0 = (f.name)[(f.name).find("WhatsApp Chat with ")::]
-    t0 = t0.replace('Chat with','ChatStats for').rstrip('.txt')
-    t0+='\n'+"Members: "+str(list(user_dict.keys())).lstrip('[').rstrip(']').replace("'",'')
-else:
-    t0="Members: "+str(list(user_dict.keys())).lstrip('[').rstrip(']').replace("'",'')
-plt.figure('WhatStats')
-plt.plot(list(tot_dates.keys()),y,label="Total",alpha=0.2)
-for i in user_dict:
-    tot_dates_copy=tot_dates.copy()
-    tot_dates_copy.update(user_dict[i])
-    plt.plot(list(tot_dates_copy.keys()),list(tot_dates_copy.values()),label=i)
-plt.xticks(rotation=45)
-plt.ylabel('Frequency of messages')
-plt.title(t0)
-plt.legend()
-plt.show()
+
+#---Calculate Time Interval---#
+def calc_interval(dlist):
+    a=datetime.datetime.strptime(dlist[0],'%b%d,%y')
+    b=datetime.datetime.strptime(dlist[-1],'%b%d,%y')
+    for j in range((b-a).days+1):
+        z=(a+datetime.timedelta(days=j))
+        tot_ints[z.strftime('%b%d,%y')]=0
+    return(tot_ints)
+
+btn_open = Button(root, text ='Open', command = open_file)
+btn_open.place(x=20,y=20)
+txt_divs = Entry(root,width=3)
+txt_divs.place(x=100,y=22)
+btn_plot = Button(root, text ='Plot', command = plot_graph)
+btn_plot.place(x=20,y=50)
+mainloop()
